fe739cf7375ffaacca55dddde98e3f4a6375abdd
minok <minok9708@gmail.com>
2025-06-17 21:27:19 +0900

    core: eslint export 관련,tailwind-config fontSize, color 수정(#3)

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,6 +4,12 @@ import { reactConfig } from './react.js';
 import { nextJsConfig } from './next.js';
 import { javascriptConfig } from './javascript.js';
 
+export { baseConfig } from './base.js';
+export { typescriptConfig } from './typescript.js';
+export { reactConfig } from './react.js';
+export { nextJsConfig } from './next.js';
+export { javascriptConfig } from './javascript.js';
+
 export const config = [
   ...baseConfig,
   ...typescriptConfig,
